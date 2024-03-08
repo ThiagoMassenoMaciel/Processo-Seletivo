3acb6c5da0a8c29e07ab35f75e9f1057b6009611
Thiago Masseno Maciel <thiagomasseno3@gmail.com>
2024-03-08 18:42:25 -0300

    q4

--- questao4.js
+++ questao4.js
@@ -10,22 +10,22 @@ controla cada lâmpada?
 */
 console.log("-------------Resposta da questão 4 é :")
 
-console.log("Se a lampada tiver quente é conectada ao interruptor que liguei e desliguei depois de 20 minutos")
+console.log(" eu posso diferenciar as três lâmpadas com estas 3 caracteristicas  uma quente, uma acesa e outra apagada  ")
 
-console.log("Se a lampada tiver acesa é conectada ao interruptor que deixei ligado")
+console.log("Ligo o primeiro interruptor e espero ums 20 minutos e desligo o mesmo interruptor, faço isso para referenciar a lâmpada quente com este interruptor")
 
-console.log("Se a lampada tiver apagada é do interruptor que eu não liguei")
+console.log("Ligo o segundo interruptor e deixo assim ligado , faço isso para que quando eu ver a lâmpada acesa saber que é controlada por este interruptor ")
 
-console.log(" eu posso diferenciar as três lampadas com estas 3 caracteristicas  uma quente, uma acesa e outra apagada  ")
+console.log("Deixo o terceiro interruptor sem ligar , para quando entrar na sala e a lâmpada tiver apagada saber que é controlada por este terceiro interruptor ")
 
-console.log("Ligo o primeiro interruptor e espero ums 20 minutos e desligo o mesmo interruptor, faço isso para referenciar a lampada quente com este interruptor")
+console.log("----Primeira ida a sala: verifico qual das 3 características é presente na lâmpada da sala que entrei e relembro do interruptor de acordo com a ordem que liguei")
 
-console.log("Ligo o segundo interruptor e deixo assim ligado , faço isso para que quando eu ver a lampada acesa saber que é controlada por este interruptor ")
+console.log("----Segunda ida a outra sala : verifico qual das 2 caracteristica tem a lâmpada da sala , com isso devo relembrar qual interruptor esta relacionado com tal característica")
 
-console.log("Deixo o terceiro interruptor sem ligar , para quando entrar na sala e a lampada tiver apagada saber que é controlada por este terceiro interruptor ")
+console.log("depois de identificar as caracteristicas com o interruptor conectado de duas lâmpadas eu ja consigo descobrir qual interruptor é conectado a ultima lâmpada , sendo aquele que sobrou.")
 
-console.log("----Primeira ida a sala: verifico qual das 3 características é presente na lampada da sala que entrei e relembro do interruptor de acordo com a ordem que liguei")
+console.log("Se a lâmpada tiver quente é conectada ao interruptor que liguei e desliguei depois de 20 minutos")
 
-console.log("----Segunda ida a outra sala : verifico qual das 2 caracteristica tem a lampada da sala , com isso devo relembrar qual interruptor esta relacionado com tal característica")
+console.log("Se a lâmpada tiver acesa é conectada ao interruptor que deixei ligado")
 
-console.log("depois de identificar as caracteristicas com o interruptor conectado de duas lampadas eu ja consigo descobrir qual interruptor é conectado a ultima lampada , sendo aquele que sobrou.")
+console.log("Se a lâmpada tiver apagada é do interruptor que eu não liguei")
